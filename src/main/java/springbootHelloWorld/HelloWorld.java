675c2fc501b182b2d48c7047b0d8ed7ada31eb90
niharika271098 <niharika271098@gmail.com>
2021-07-18 19:19:09 -0400

    updated dev branch

--- src/main/java/springbootHelloWorld/HelloWorld.java
+++ src/main/java/springbootHelloWorld/HelloWorld.java
@@ -12,7 +12,7 @@ public class HelloWorld{
 	@RequestMapping("/")
 	@ResponseBody
 	String home() {
-		return "Hello World";
+		return "Hello World from dev branch";
 	}
 	
 	public static void main(String[]args) throws Exception {
